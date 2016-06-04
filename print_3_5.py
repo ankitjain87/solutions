21bf2a6431fb53c2039a52f7278e148c3286f4b5
ankitjain87 <ankit.it09@gmail.com>
2016-06-04 09:53:34 +0530

    Update print_3_5.py

--- print_3_5.py
+++ print_3_5.py
@@ -1,5 +1,7 @@
+"""Write a program that prints the numbers from 1 to 100. But for multiples of three print “Three” instead of the number and for the multiples of five print “Five”. For numbers which are multiples of both three and five print “ThreeFive”."""
 
-def print_number(n):
+
+def print_number(n=100):
     result = []
     for i in range(1, n+1):
         if i%15 == 0:
